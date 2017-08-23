ff1a4bc0dd64258a67897cc67945aacb0f88b7ea
Cccshine <1071762488@qq.com>
2017-08-23 23:16:39 +0800

    add register judge

--- views/src/pages/register.js
+++ views/src/pages/register.js
@@ -14,22 +14,24 @@ class Login extends React.Component{
 	      userNameErr:'',
 	      userNamePass:false,
 	      userNameInfo:'只包含汉字、数字、字母、下划线其中一种或几种，长度不能超过10',
-	      passowrdErr:'',
-	      passowrdPass:false,
-	      passowrdInfo:'必须包含数字、字母、下划线其中两种，长度不小于8'
+	      passwordErr:'',
+	      passwordPass:false,
+	      passwordInfo:'必须包含数字、字母、下划线其中两种，长度不小于8',
+	      comfirmPasswordErr:'',
+	      comfirmPasswordPass:false
 	    };
 	}
 	handleUserNameChange = (event) => {
 		var value = event.target.value;
 		this.setState({userName:value});
 		if(value === ''){
-			this.setState({userNameErr:'用户名不能为空'});
+			this.setState({userNameErr:'用户名不能为空',userNamePass:false});
 			return;
 		}else if(value.length > 10){
-			this.setState({userNameErr:'用户名长度不能超过10个字符'});
+			this.setState({userNameErr:'用户名长度不能超过10个字符',userNamePass:false});
 			return;
 		}else if(value.match(/[^a-zA-Z0-9_\u4e00-\u9fa5]+/)){//只含有汉字、数字、字母、下划线
-			this.setState({userNameErr:'用户名只能含有汉字、数字、字母、下划线'});
+			this.setState({userNameErr:'用户名只能含有汉字、数字、字母、下划线',userNamePass:false});
 			return;
 		}else{
 			this.setState({userNamePass:true});
@@ -38,19 +40,36 @@ class Login extends React.Component{
 	handlePasswordChange = (event) => {
 		var value = event.target.value;
 		this.setState({password:value});
-		event.preventDefault();
+		if(value === ''){
+			this.setState({passwordErr:'密码不能为空',passwordPass:false});
+			return;
+		}else if(value.length < 8){
+			this.setState({passwordErr:'密码长度不能少于8个字符',passwordPass:false});
+			return;
+		}else if(value.match(/^[0-9]+$/) || value.match(/^[a-zA-Z]+$/) || value.match(/^[_]+$/)){
+			this.setState({passwordErr:'密码必须包含数字、字母、下划线其中两种',passwordPass:false});
+			return;
+		}else{
+			this.setState({passwordPass:true});
+		}
 	}
 	handleComfirmPasswordChange = (event) => {
 		var value = event.target.value;
-		this.setState({comfirmPassword:value});
-		event.preventDefault();
+		// this.setState({comfirmPassword:value});
+		// if(value !== this.state.password){
+		// 	this.setState({comfirmPasswordErr:'两次输入密码不一致',comfirmPasswordPass:false});
+		// 	return;
+		// }
+		// this.setState({comfirmPasswordErr:'',comfirmPasswordPass:true});
 	}
 	handleSubmit = (event) => {
 		event.preventDefault();
 	}
 	render(){
-		var tipType = this.state.userNameErr === '' ? 'info' : (this.state.userNamePass ? 'success' : 'error');
-		var tipText = this.state.userNamePass ?  this.state.userNameErr : (this.state.userNameErr === '' ? this.state.userNameInfo : '符合规则');
+		var userNameTipType = this.state.userNamePass ?  'success' : (this.state.userNameErr === '' ? 'info' : 'error');
+		var userNameTipText = this.state.userNamePass ?  '符合规则' : (this.state.userNameErr === '' ? this.state.userNameInfo : this.state.userNameErr);
+		var passwordTipType = this.state.passwordPass ?  'success' : (this.state.passwordErr === '' ? 'info' : 'error');
+		var passwordTipText = this.state.passwordPass ?  '符合规则' : (this.state.passwordErr === '' ? this.state.passwordInfo : this.state.passwordErr);
 		return(
 			<div>
 				<QuickLink pageName="register"/>
@@ -58,15 +77,15 @@ class Login extends React.Component{
 					<form onSubmit={this.handleSubmit}>
 						<div styleName="form-group" className="fa fa-user">
 							<input type="text" name="username" id="username" placeholder="用户名" onChange={this.handleUserNameChange}/>
-							<TipBar type={tipType} text={tipText}/>
+							<TipBar type={userNameTipType} text={userNameTipText}/>
 						</div>
 						<div styleName="form-group" className="fa fa-lock">
 							<input type="password" name="password" id="password" placeholder="密码" onChange={this.handlePasswordChange}/>
-							<TipBar type={tipType} text={tipText}/>
+							<TipBar type={passwordTipType} text={passwordTipText}/>
 						</div>
 						<div styleName="form-group" className="fa fa-lock">
 							<input type="password" name="password"  placeholder="确认密码" onChange={this.handleComfirmPasswordChange}/>
-							<TipBar type={tipType} text={tipText}/>
+							// {this.state.comfirmPasswordErr == '' ? '' : <TipBar type="error" text={this.state.comfirmPasswordErr}/>}
 						</div>
 						<button styleName="operate-btn" name="login-btn">注册</button>
 					</form>
