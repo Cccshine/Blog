dd7d03c18b2a6d050b9a5e570a7d855ba11f18b6
曹嘉聪 <1071762488@qq.com>
2018-02-26 18:36:24 +0800

    M 完成用户中心界面

---
+++ public/src/pages/thumbs-up.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import moment from 'moment';
+import marked from 'marked';
+import Modal from '../component/modal/modal';
+import Tag from '../component/tag/tag';
+import CSSModules from 'react-css-modules';
+import style from '../sass/pages/user.scss';
+import blogGlobal from '../data/global';
+
+class ThumbUp extends React.Component{
+	constructor(props){
+		super(props);
+	}
+	render(){
+		let tagProps = { isLink: true, hasClose: false };
+		let list = ["css","html"]
+		return(
+			<div styleName="profile-active-list">
+				点赞
+				<section styleName="list-item">
+					<div styleName="list-item-content">
+						<div className="clearfix">
+							<h3 className="fl"><Link target="_blank" to={"/articles/" + '1'}>xxxxxx</Link></h3>
+						</div>
+						<div styleName="tag-panel">
+							<Tag {...tagProps} list={list} />
+						</div>
+						<div styleName="summary">
+							<p dangerouslySetInnerHTML={{ __html: marked('svdchgdvchsddddcdfvhdfv') }}></p>
+						</div>
+						<div className="clearfix">
+							<div styleName="article-info" className="fl">
+								<div><i className="fa fa-thumbs-up"></i><span>赞(5)</span></div>
+								<div><i className="fa fa-bookmark"></i><span>收藏(5)</span></div>
+								<div><i className="fa fa-commenting"></i><span>评论(5)</span></div>
+							</div>
+							<div styleName="more" className="fr"><Link target="_blank" to={"/articles/" + '1'}>阅读全文</Link></div>
+						</div>
+					</div>
+				</section>
+				<section styleName="list-item">
+					<div styleName="list-item-content">
+						<div className="clearfix">
+							<h3 className="fl"><Link target="_blank" to={"/articles/" + '1'}>xxxxxx</Link></h3>
+						</div>
+						<div styleName="tag-panel">
+							<Tag {...tagProps} list={list} />
+						</div>
+						<div styleName="summary">
+							<p dangerouslySetInnerHTML={{ __html: marked('svdchgdvchsddddcdfvhdfv') }}></p>
+						</div>
+						<div className="clearfix">
+							<div styleName="article-info" className="fl">
+								<div><i className="fa fa-thumbs-up"></i><span>赞(5)</span></div>
+								<div><i className="fa fa-bookmark"></i><span>收藏(5)</span></div>
+								<div><i className="fa fa-commenting"></i><span>评论(5)</span></div>
+							</div>
+							<div styleName="more" className="fr"><Link target="_blank" to={"/articles/" + '1'}>阅读全文</Link></div>
+						</div>
+					</div>
+				</section>
+			</div>
+		)
+	}
+}
+export default CSSModules(ThumbUp, style, { handleNotFoundStyleName: 'log' });
--- public/src/pages/user.js
+++ public/src/pages/user.js
@@ -1,22 +1,58 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Route,Switch} from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import moment from 'moment';
+import marked from 'marked';
 import Modal from '../component/modal/modal';
 import Tag from '../component/tag/tag';
 import CSSModules from 'react-css-modules';
 import style from '../sass/pages/user.scss';
 import blogGlobal from '../data/global';
+import Activity from './activity.js';
+import Collection from './collection.js';
+import ThumbUp from './thumbs-up.js';
+import NoMatch from './nomatch.js';
 
-export default class User extends React.Component{
+class User extends React.Component{
 	constructor(props){
 		super(props);
 	}
+
+	componentWillMount = () => {
+		this.props.history.push({ pathname: '/user/activities'});
+	}
+
 	render(){
+		let tagProps = { isLink: true, hasClose: false };
+		let list = ["css","html"]
 		return(
 			<div styleName="root">
-				
+				<header className="clearfix" styleName="profile-header">
+					<div className="fl" styleName="user-info">
+						<img className="fl" src={require('../images/logo.jpg')}  alt="cshine"/>
+						<h3 className="fl">Cshine</h3>
+					</div>
+					<div className="fr" styleName="btn-group">
+						<button className="btn-normal btn-md">设置</button>
+					</div>
+				</header>
+				<div styleName="profile-main">
+					<nav styleName="profile-tabs" className="profile-tabs">
+						<NavLink to="/user/activities" activeClassName="active-tab">动态</NavLink>
+						<NavLink to="/user/collections" activeClassName="active-tab">收藏</NavLink>
+						<NavLink to="/user/thumbs-up" activeClassName="active-tab">点赞</NavLink>
+					</nav>
+					<div>
+						<Switch>
+							<Route path="/user/collections" component={Collection}/>
+							<Route path="/user/thumbs-up" component={ThumbUp}/>
+							<Route path="/user" component={Activity}/>
+							<Route component={NoMatch}/>
+					    </Switch>
+					</div>
+				</div>
 			</div>
 		)
 	}
 }
-export default CSSModules(User, style, { allowMultiple: true, handleNotFoundStyleName: 'log' });
+export default CSSModules(User, style, { handleNotFoundStyleName: 'log' });
